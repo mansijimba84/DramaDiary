b9bbf2579bf90544c18e23fb37969a887eefabcc
Mansi <mansezimba14@gmail.com>
2026-06-07 16:22:13 +0545

    client

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <section id="center">
+      <div className="hero-text">
+        <h1>DramaDiary</h1>
+
+        <h3>Your K-drama journal</h3>
+
+        <p>
+          Coming soon... 🎬✨  
+          Track your favorite K-dramas, save reviews, and relive every emotional moment.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default App
