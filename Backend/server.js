b122046098f50a4a76e1ed218913bbd5aa228d30
Mansi <mansezimba14@gmail.com>
2026-06-15 23:48:29 +0545

    fixed the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,8 @@
-require ('dotenv').config()
-const app = require("./index")
-const PORT = process.env.PORT
-app.listen(
-    5050, //port -> localhost:5050
-    () => {
-        console.log("Server started")
-    }
-)
+require("dotenv").config();
+const app = require("./index");
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
